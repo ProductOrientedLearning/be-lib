9064503f02cba54abdf4425a3f3bb56ecc71bf57
Nguyen Manh Ha <manhhad32@gmail.com>
2023-10-13 09:39:34 +0700

    init project

---
+++ src/main/java/pol/ecom/miro/shop/lib/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package pol.ecom.miro.shop.lib.config;
+/*
+ * This is course Microservice Product Oriented
+ * MIT No Attribution
+
+ * Copyright (c) 2023 <Dr.JohnLe & Mr.HaNguyen>
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    RequestFilter requestFilter;
+
+    @Autowired
+    public SecurityConfig(RequestFilter requestFilter) {
+        this.requestFilter = requestFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> {auth
+                            .requestMatchers(HttpMethod.GET, "/*").permitAll()
+                            .requestMatchers("/swagger-resources/**").permitAll()
+                            .requestMatchers("/v3/api-docs/**").permitAll()
+                            .requestMatchers("/swagger-ui/**").permitAll()
+                            .requestMatchers("/actuator/**").permitAll()
+                            .requestMatchers("/**").authenticated();
+                })
+                .addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+        ;
+        return httpSecurity.build();
+    }
+}
